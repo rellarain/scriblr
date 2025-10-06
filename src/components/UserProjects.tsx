6b4f30b22329a3af48d95bbc80b318336c8dfb0d
rellarain <aurellarainwalker@gmail.com>
2025-10-05 22:06:00 -0400

    10/5/25 10PM

--- src/components/UserProjects.tsx
+++ src/components/UserProjects.tsx
@@ -22,6 +22,7 @@ function handleReaderProjectOpen() {
 
 return (<main className={"userProjectsUI open"+userProjectsState}>
     <section>Account Dashboard</section>
+    <Admin/>
     <section>
         <div onClick={handleReaderProjectOpen}>
             <button></button>
@@ -35,7 +36,18 @@ return (<main className={"userProjectsUI open"+userProjectsState}>
 
         </div>
         <div onClick={handleReaderProjectOpen}></div>
-        <div>Reader Project Dash</div>
+        <div>
+            <br/>Reader Project Dash
+            <br/>
+            <br/>
+            <br/>
+            <br/>Alpha Reader Dash: can view outline, draft, and annotations; progress based on outline and draft review with or without flags/notes (unreviewed, updated, reviewed)
+            <br/>
+            <br/>Beta Reader Dash: can view draft and annotations; progress based on draft review with or without flags/notes (related to structure, content, and/or draft material)
+            <br/>
+            <br/>Omega Reader Dash: can view draft (formatted or arrayed); progress based on draft review with or without flags/notes (only grammatical, rhetorical, syntactical, commentary; chapter tabs without arcs/subarcs)
+            <br/>
+        </div>
     </section>
     <section>
         <div onClick={handleWriterProjectOpen}>
@@ -45,7 +57,7 @@ return (<main className={"userProjectsUI open"+userProjectsState}>
             <button></button>
             <button></button>
         </div>
-        <div onClick={handleWriterProjectOpen}></div>
+        <div onClick={handleWriterProjectOpen}>drag and droppable books to change order</div>
         <div onClick={handleWriterProjectOpen}></div>
         <div onClick={handleWriterProjectOpen}>
             <button></button>
@@ -54,12 +66,17 @@ return (<main className={"userProjectsUI open"+userProjectsState}>
         <div>
             <br/>Writer Project Dash (Project Level, Book Level)
             <br/>
-            <br/>Project Level: 
+            <br/>Project Level: content (plotlines, book sequences), multibook structures (sequels, prequels, trilogies, sagas, duologies), series drafting progress and plan
             <br/>
-            <br/>Book Level: manage details for book, arcs, subarcs; arc and chapter 
+            <br/>Book Level: manage details for book, arcs, subarcs; plot sequences;
+            <br/>
+            <br/>Chapter Level: manage details for acts, scenes, moments; manage plot sequences, plotpoints; 
+            <br/>
+            <br/>Draft Level: manage draft, annotations; 
+            <br/>
+            <br/>Reader Level: review and revise draft (alpha, beta, omega) 
         </div>
     </section>
-    <Admin/>
 </main>)}
 
 export default UserProjects;
