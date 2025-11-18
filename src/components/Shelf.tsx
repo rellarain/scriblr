7c49580ceb8576286eee27dbc43c23f4fcbe5738
RellaRain <aurellarainwalker@gmail.com>
2025-11-17 19:57:12 -0500

    11/17/25 8 PM

---
+++ src/components/Shelf.tsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+
+function Shelf() {
+
+
+
+
+
+
+return (<div className="shelfUI">
+    <button>Project</button>
+
+    <div>
+        <button>BK1</button>
+        <button>BK2</button>
+        <button>BK3</button>
+        <button>BK3</button>
+        <button>BK3</button>
+        <button>BK3</button>
+        <button>BK3</button>
+        <button>BK3</button>
+        <button>BK3</button>
+        <button>BK3</button>
+        <button>BK3</button>
+        <button>BK3</button>
+        <button>BK3</button>
+
+    </div>
+
+
+
+
+</div>)}
+
+
+export default Shelf;
---
+++ src/components/Shelves.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import Shelf from "./Shelf";
+
+
+function Shelves() {
+
+
+
+
+
+
+return (<main className="shelvesUI">
+    <Shelf/>
+    <Shelf/>
+    <Shelf/>
+
+</main>)}
+
+
+export default Shelves;
